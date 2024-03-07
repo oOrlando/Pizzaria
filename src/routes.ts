772d752e2c3bf4cefa0f1416315d5829f7a776e2
Orlando de Oliveira Junior <orlando.oliveira@aluno.ifsp.edu.br>
2024-03-07 20:56:00 -0300

    Funcionalidades de produtos finalizados

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ import { isAuthenticated } from './middlewares/isAuthenticated'
 import { CreateCategoryController } from './controllers/category/CreateCategoryController'
 import { ListCategoryController } from './controllers/category/ListCategoryController'
 import { CreateProductController } from './controllers/product/CreateProductController'
+import { ListByCategoryController } from './controllers/product/ListByCategoryController'
 
 import uploadConfig from './config/multer'
 
@@ -29,7 +30,7 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 // -- ROTAS PRODUCT
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
-
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
 
 export { router };
