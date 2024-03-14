b9ac7329adc798699e537bba55d42a1e8c1619e1
Orlando de Oliveira Junior <orlando.oliveira@aluno.ifsp.edu.br>
2024-03-13 21:53:47 -0300

    Criadas rotas para abir order e deletar order

---
+++ src/controllers/order/CreateOrderController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { CreateOrderService } from "../../services/order/CreateOrderService";
+
+class CreateOrderController{
+
+    async handle(req: Request, res: Response){
+        const {table, name} = req.body
+        const createorderService = new CreateOrderService();        
+
+        const order = await createorderService.execute({
+            table,
+            name
+        })
+
+        return res.json(order)
+
+
+
+    }
+
+}
+
+export { CreateOrderController}
--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,8 @@ import { CreateCategoryController } from './controllers/category/CreateCategoryC
 import { ListCategoryController } from './controllers/category/ListCategoryController'
 import { CreateProductController } from './controllers/product/CreateProductController'
 import { ListByCategoryController } from './controllers/product/ListByCategoryController'
+import { CreateOrderController } from './controllers/order/CreateOrderController'
+import { RemoveOrderController } from './controllers/order/RemoveOrderController'
 
 import uploadConfig from './config/multer'
 
@@ -31,6 +33,11 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle)
 // -- ROTAS PRODUCT
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
+
+//-- ROTAS ORDER
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+
 
 
 export { router };
