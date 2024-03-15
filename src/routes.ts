bcb8f245d1f6aecc9a551514f4617a8c1c4eeb55
Orlando de Oliveira Junior <orlando.oliveira@aluno.ifsp.edu.br>
2024-03-14 22:26:50 -0300

    Funcionalidades de pedidos finalizadas

--- src/routes.ts
+++ src/routes.ts
@@ -11,9 +11,14 @@ import { CreateProductController } from './controllers/product/CreateProductCont
 import { ListByCategoryController } from './controllers/product/ListByCategoryController'
 import { CreateOrderController } from './controllers/order/CreateOrderController'
 import { RemoveOrderController } from './controllers/order/RemoveOrderController'
+import { AddItemController } from './controllers/order/AddItemController'
+import { RemoveItemController } from './controllers/order/RemoveItemController'
+import { SendOrderController } from './controllers/order/SendOrderController'
+import { ListOrdersController } from './controllers/order/ListOrdersController'
+import { DetailOrdersController } from './controllers/order/DetailOrdersController'
+import { FinishOrderController } from './controllers/order/FinishOrderController'
 
 import uploadConfig from './config/multer'
-
 
 const router = Router();
 
@@ -37,6 +42,12 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 //-- ROTAS ORDER
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
+router.get('/orders', isAuthenticated, new ListOrdersController().handle)
+router.get('/order/datail', isAuthenticated, new DetailOrdersController().handle)
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle)
 
 
 
